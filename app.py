90359f3d978a8c2a5b3a82c57d0f91a96b860e75
Gaurav Pandey <167669943+Gaurav-1111@users.noreply.github.com>
2025-03-13 11:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,21 @@ st.set_page_config(page_title = "Prediction of Diseases Outbreaks",
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
+'''
 diabetes_model = pickle.load(open(r"Prediction-of-Diseases-Outbreak/Prediction of Disease/Diabetes_model.sav",'rb'))
 heart_model = pickle.load(open(r"Prediction-of-Diseases-Outbreak/Prediction of Disease/heart_disease_model.sav",'rb'))
 parkinson_model = pickle.load(open(r"Prediction-of-Diseases-Outbreak/Prediction of Disease/parkinson_model.sav",'rb'))
+'''
+
+with open('Prediction-of-Diseases-Outbreak/Prediction of Disease/Diabetes_model.sav', 'rb') as file:
+  diabetes_model = pickle.load(file)
+with open('Prediction-of-Diseases-Outbreak/Prediction of Disease/heart_diseases_model.sav', 'rb') as file:
+  diabetes_model = pickle.load(file)
+with open('Prediction-of-Diseases-Outbreak/Prediction of Disease/parkinson_model.sav', 'rb') as file:
+  diabetes_model = pickle.load(file)
+
+# Use the model to make predictions (example with test data)
+predictions = loaded_model.predict(X_test) 
 
 with st.sidebar:
     selected = option_menu('Prediction of Diseases Outbreaks System ',
